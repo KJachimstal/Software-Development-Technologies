b8565860d8e8b15f70e4fb43ce0d75a2fe606aa3
k.jachimstal <k.jachimstal@gmail.com>
2019-05-14 19:44:05 +0200

    Change namespace to CasinoLibrary

--- Casino/Casino/Game.cs
+++ Casino/Casino/Game.cs
@@ -1,34 +1,37 @@
 ﻿using System;
 
-public class Game
+namespace CasinoLibrary
 {
-    public enum GameType { POKER, BRIDGE, ROULETTE }
-
-    private int id;
-
-    public int Id {
-        get { return id; }
-        set { id = value; }
-    }
-
-    private string name;
-
-    public string Name {
-        get { return name; }
-        set { name = value; }
-    }
-
-    private GameType type;
-
-    public GameType Type {
-        get { return type; }
-        set { type = value; }
-    }
-
-    public Game(int id, string name, GameType type)
+    public class Game
     {
-        this.id = id;
-        this.name = name;
-        this.type = type;
+        public enum GameType { POKER, BRIDGE, ROULETTE }
+
+        private int id;
+
+        public int Id {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private GameType type;
+
+        public GameType Type {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public Game(int id, string name, GameType type)
+        {
+            this.id = id;
+            this.name = name;
+            this.type = type;
+        }
     }
 }
