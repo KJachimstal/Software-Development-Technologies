e814bfa72990511f4286e9edf3510b5b6c9c5816
k.jachimstal <k.jachimstal@gmail.com>
2019-05-14 18:52:02 +0200

    Initialize GameDetails

---
+++ Casino/Casino/GameDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class GameDetails
+    {
+        private Game game;
+
+        public Game Game {
+            get { return game; }
+            set { game = value; }
+        }
+    }
+}
