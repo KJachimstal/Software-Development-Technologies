b3ccecedc39c67407a2b0651646844a87b220734
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-14 19:03:17 +0200

    Initialize participation class

---
+++ Casino/Casino/Participation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Participation
+    {
+        private Client client;
+
+        public Client Client {
+            get { return client; }
+            set { client = value; }
+        }
+
+    }
+}
