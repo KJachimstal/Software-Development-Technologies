0553cf551bdb6820f8d14b2ac87a054a761259e4
k.jachimstal <k.jachimstal@gmail.com>
2019-05-14 19:45:10 +0200

    Initialize DataContext file

--- Casino/CasinoData/DataContext.cs
+++ Casino/CasinoData/DataContext.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CasinoLibrary;
 
 namespace CasinoData
 {
     class DataContext
     {
-       
+        private List<Client> clients;
+
+        public List<Client> Clients {
+            get { return clients; }
+            set { clients = value; }
+        }
+
     }
 }
