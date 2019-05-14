9f9453334eea2218c2d0325738a8388a51e146de
k.jachimstal <k.jachimstal@gmail.com>
2019-05-14 20:03:49 +0200

    Add constructor to DataContext

--- Casino/CasinoData/DataContext.cs
+++ Casino/CasinoData/DataContext.cs
@@ -38,6 +38,12 @@ namespace CasinoData
             set { participation = value; }
         }
 
-
+        public DataContext(List<Client> clients, Dictionary<int, Game> games, ObservableCollection<GameDetails> gameDetails, List<Participation> participation)
+        {
+            this.clients = clients;
+            this.games = games;
+            this.gameDetails = gameDetails;
+            this.participation = participation;
+        }
     }
 }
