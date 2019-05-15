4e21b6508e8828d6a5b99573f484cd281da45ff3
k.jachimstal <k.jachimstal@gmail.com>
2019-05-15 20:32:57 +0200

    Add GameDetailsService methods

--- Casino/CasinoData/DataService/GameDetailsService.cs
+++ Casino/CasinoData/DataService/GameDetailsService.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CasinoLibrary;
+using System.Collections.ObjectModel;
 
 namespace CasinoData
 {
     public partial class DataService
     {
+        public string PrintGameDetails(ObservableCollection<GameDetails> gameDetails)
+        {
+            string gameDetailsString = "";
+
+            for (int i = 0; i <= gameDetails.Count; i++)
+            {
+                gameDetailsString += gameDetails;
+
+                if (i + 1 != gameDetails.Count)
+                {
+                    gameDetailsString += ", ";
+                }
+            }
+
+            return gameDetailsString;
+        }
+
+        public void AddGameDetails(GameDetails gameDetails)
+        {
+            if (!dataRepository.GetAllGameDetails().Contains(gameDetails))
+            {
+                dataRepository.AddGameDetails(gameDetails);
+            } else
+            {
+                throw new Exception("GameDetails alredy exists!");
+            }
+        }
+
+        public void DeleteGameDetails(GameDetails gameDetails)
+        {
+            if (dataRepository.GetAllGameDetails().Contains(gameDetails))
+            {
+                dataRepository.DeleteGameDetails(gameDetails);
+            }
+            else
+            {
+                throw new Exception("GameDetails does not exists!");
+            }
+        }
+
+        public void UpdateGameDetails(GameDetails oldGameDetails, GameDetails newGameDetails)
+        {
+            if (dataRepository.GetAllGameDetails().Contains(oldGameDetails))
+            {
+                dataRepository.UpdateGameDetails(oldGameDetails, newGameDetails);
+            }
+            else
+            {
+                throw new Exception("GameDetails does not exists!");
+            }
+        }
     }
 }
