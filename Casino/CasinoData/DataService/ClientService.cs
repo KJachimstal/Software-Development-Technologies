c2470b5a5ca07bbeb4be43a24dcd2bea7b9f64d3
k.jachimstal <k.jachimstal@gmail.com>
2019-05-15 20:06:38 +0200

    Add ClientService

--- Casino/CasinoData/DataService/ClientService.cs
+++ Casino/CasinoData/DataService/ClientService.cs
@@ -3,11 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CasinoLibrary;
 
 namespace CasinoData
 {
     public partial class DataService
     {
+        public string PrintClients(List<Client> clients)
+        {
+            string clientsString = "";
 
+            foreach (Client client in clients)
+            {
+                clientsString += client;
+                if (clients.LastIndexOf(client) != clients.Count)
+                {
+                    clientsString += ", ";
+                }
+            }
+
+            return clientsString;
+        }
+
+        public void AddClient(Client client)
+        {
+            if (!dataRepository.GetAllClients().Contains(client))
+            {
+                dataRepository.AddClient(client);
+            } else
+            {
+                throw new Exception("Client alredy exists!");
+            }
+        }
+
+        public void DeleteClient(Client client)
+        {
+            if (dataRepository.GetAllClients().Contains(client))
+            {
+                dataRepository.DeleteClient(client);
+            } else
+            {
+                throw new Exception("Client not found!");
+            }
+        }
+
+        public void UpdateClient(Client oldClient, Client newClient)
+        {
+            if (dataRepository.GetAllClients().Contains(oldClient))
+            {
+                dataRepository.UpdateClient(oldClient, newClient);
+            } else
+            {
+                throw new Exception("Client not found! You can not change.");
+            } 
+        }
     }
 }
