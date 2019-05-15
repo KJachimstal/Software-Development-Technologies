68138d808fd7532a96b6e32e262f444a8927d50e
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-15 12:28:39 +0200

    Add addParticipation method to participations repository

--- Casino/CasinoData/DataRepository/ParticipationsRepository.cs
+++ Casino/CasinoData/DataRepository/ParticipationsRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CasinoLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace CasinoData
 {
     public partial class DataRepository
     {
+        public void AddParticipation(Participation participation)
+        {
+            dataContext.Participations.Add(participation);
+        }
     }
 }
