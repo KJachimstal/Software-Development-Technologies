383f2486ea66695ed08a53246eccda1d54b9dd2e
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-15 12:20:49 +0200

    Add addGame method to games repository

--- Casino/CasinoData/DataRepository/GamesRepository.cs
+++ Casino/CasinoData/DataRepository/GamesRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CasinoLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace CasinoData
 {
     public partial class DataRepository
     {
+        public void AddGame(Game game)
+        {
+            dataContext.Games.Add(game.Id, game);
+        }
+
     }
 }
