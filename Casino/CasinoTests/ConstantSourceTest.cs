ef04d68a57dd5aa72b0d1dcb93d60aa4afe65d64
k.jachimstal <k.jachimstal@gmail.com>
2019-05-15 10:57:19 +0200

    Initialize ConstantSourceTest

---
+++ Casino/CasinoTests/ConstantSourceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CasinoData;
+using CasinoSources;
+
+namespace CasinoTests
+{
+    [TestClass]
+    public class ConstantSourceTest
+    {
+        [TestMethod]
+        public void TestConstantSource()
+        {
+
+        }
+    }
+}
