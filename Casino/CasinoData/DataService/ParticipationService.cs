bac42dcd9a5a744fe511c0bef5c9615c21eb87e5
k.jachimstal <k.jachimstal@gmail.com>
2019-05-15 20:42:50 +0200

    Add ParticipationService methods

--- Casino/CasinoData/DataService/ParticipationService.cs
+++ Casino/CasinoData/DataService/ParticipationService.cs
@@ -3,10 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CasinoLibrary;
 
 namespace CasinoData
 {
     public partial class DataService
     {
+        public string PrintParticipation(List<Participation> participations)
+        {
+            string participationString = "";
+
+            foreach (Participation participation in participations)
+            {
+                participationString += participation;
+
+                if (participations.LastIndexOf(participation) != participations.Count)
+                {
+                    participationString += ", ";
+                }
+            }
+
+            return participationString;
+        }
+
+        public void AddParticipations(Participation participation)
+        {
+            if (!dataRepository.GetAllParticipations().Contains(participation))
+            {
+                dataRepository.AddParticipation(participation);
+            } else
+            {
+                throw new Exception("Participation alredy exists!");
+            }
+        }
+
+        public void DeleteParticipation(Participation participation)
+        {
+            if (dataRepository.GetAllParticipations().Contains(participation))
+            {
+                dataRepository.DeleteParticipation(participation);
+            }
+            else
+            {
+                throw new Exception("Participation does not exists!");
+            }
+        }
+
+        public void UpdateParticipation(Participation oldParticipation, Participation newParticipation)
+        {
+            if (dataRepository.GetAllParticipations().Contains(oldParticipation))
+            {
+                dataRepository.UpdateParticipation(oldParticipation, newParticipation);
+            }
+            else
+            {
+                throw new Exception("Participation does not exists!");
+            }
+        }
     }
 }
