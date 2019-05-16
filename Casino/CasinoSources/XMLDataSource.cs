5a6fcea908cb2647569d09743fade748eaaedf81
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-16 13:35:12 +0200

    Initialize xml data source

---
+++ Casino/CasinoSources/XMLDataSource.cs
@@ -0,0 +1,17 @@
+﻿using CasinoData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasionSources
+{
+    class XMLDataSource : IDataSource
+    {
+        public void Fill(DataContext dataContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
