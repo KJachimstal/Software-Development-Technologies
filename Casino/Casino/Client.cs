a15aa33813052f4dcd94b5499622f45e6d7b7d16
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-16 16:45:39 +0200

    Add serialization to client class

--- Casino/Casino/Client.cs
+++ Casino/Casino/Client.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace CasinoLibrary
 {
+    [Serializable()]
     public class Client
     {
         private int clientNumber;
 
+        [XmlElement("ClientNumber")]
         public int ClientNumber {
             get { return clientNumber; }
             set { clientNumber = value; }
@@ -18,6 +21,7 @@ namespace CasinoLibrary
 
         private string firstName;
 
+        [XmlElement("FirstName")]
         public string FirstName {
             get { return firstName; }
             set { firstName = value; }
@@ -25,6 +29,7 @@ namespace CasinoLibrary
 
         private string lastName;
 
+        [XmlElement("LastName")]
         public string LastName {
             get { return lastName; }
             set { lastName = value; }
