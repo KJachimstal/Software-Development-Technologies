47f5e129cc511c54aefdece3789303c87de9f282
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-16 21:04:04 +0200

    Add games list class

---
+++ Casino/CasinoSources/GamesList.cs
@@ -0,0 +1,22 @@
+﻿using CasinoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CasionSources
+{
+    [XmlRoot("GamesList")]
+    class GamesList
+    {
+        public GamesList(List<Game> games)
+        {
+            Games = games;
+        }
+
+        [XmlElement("Game")]
+        public List<Game> Games { get; set; }
+    }
+}
