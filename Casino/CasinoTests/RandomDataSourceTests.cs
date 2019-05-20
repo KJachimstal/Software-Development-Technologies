6654bf90d85858616ae3fbb3d73aa36ae99633c8
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-20 18:01:41 +0200

    Add random data source tests with different multipliers

--- Casino/CasinoTests/RandomDataSourceTests.cs
+++ Casino/CasinoTests/RandomDataSourceTests.cs
@@ -23,5 +23,29 @@ namespace CasinoTests
             IDataSource dataSource = new RandomDataSource();
             dataSource.Fill(dataContext);
         }
+
+        [TestMethod]
+        public void LargeMultiplier()
+        {
+            // 50 000x clients, 40 000x games, 30 000x games details, 80 000x participations
+            IDataSource dataSource = new RandomDataSource(100);
+            dataSource.Fill(dataContext);
+        }
+
+        [TestMethod]
+        public void BigMultiplier()
+        {
+            // 500 000x clients, 400 000x games, 300 000x games details, 800 000x participations
+            IDataSource dataSource = new RandomDataSource(1000);
+            dataSource.Fill(dataContext);
+        }
+
+        [TestMethod]
+        public void HugeMultiplier()
+        {
+            // 5 000 000x clients, 4 000 000x games, 3 000 000x games details, 8 000 000x participations
+            IDataSource dataSource = new RandomDataSource(10000);
+            dataSource.Fill(dataContext);
+        }
     }
 }
