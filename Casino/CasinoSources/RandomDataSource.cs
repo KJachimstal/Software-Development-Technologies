3a74890b205d39a951d45dcea88bc0389cf47912
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-20 16:17:19 +0200

    Add fillGames method in random data source

--- Casino/CasinoSources/RandomDataSource.cs
+++ Casino/CasinoSources/RandomDataSource.cs
@@ -10,13 +10,14 @@ namespace CasionSources
 {
     class RandomDataSource : IDataSource
     {
-
+        private DataContext dataContext;
         private int multiplier = 1;
         private static Random random = new Random();
         const string chars = "abcdefghijklmnopqrstuvwxyz";
 
         public void Fill(DataContext dataContext)
         {
+            this.dataContext = dataContext;
             FillClients();
             FillGames();
             FillGamesDetails();
@@ -25,6 +26,7 @@ namespace CasionSources
 
         private void FillClients()
         {
+            dataContext.Clients = new List<Client>();
             for (int i = 1; i <= 500 * multiplier; i++)
             {
                 Client client = new Client()
@@ -33,12 +35,23 @@ namespace CasionSources
                     FirstName = GetRandomString(random.Next(5, 10)),
                     LastName = GetRandomString(random.Next(5, 15)),
                 };
+                dataContext.Clients.Add(client);
             }
         }
 
         private void FillGames()
         {
-
+            dataContext.Games = new Dictionary<int, Game>();
+            for (int i = 1; i <= 400 * multiplier; i++)
+            {
+                Game game = new Game()
+                {
+                    Id = i,
+                    Name = GetRandomString(random.Next(5, 10)),
+                    Type = Game.GameType.POKER,
+                };
+                dataContext.Games.Add(i, game);
+            }
         }
 
         private void FillGamesDetails()
