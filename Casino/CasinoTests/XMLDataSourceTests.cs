d315f3caf9bf059986d46b782e613d903ab1c39d
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-20 15:21:38 +0200

    Add valid clients test in xml data source tests

--- Casino/CasinoTests/XMLDataSourceTests.cs
+++ Casino/CasinoTests/XMLDataSourceTests.cs
@@ -12,19 +12,34 @@ namespace CasinoTests
     public class XMLDataSourceTests
     {
 
+        private DataContext dataContext;
+        private XMLDataSource xmlSource;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            dataContext = new DataContext();
+            xmlSource = new XMLDataSource();
+            xmlSource.Fill(dataContext);
+        }
+
         [TestMethod]
         public void Fill()
         {
-            DataContext dataContext = new DataContext();
-            XMLDataSource xmlSource = new XMLDataSource();
-
-            xmlSource.Fill(dataContext);
-
             //Assertions
             Assert.AreEqual(6, dataContext.Clients.Count);
             Assert.AreEqual(3, dataContext.Games.Count);
             Assert.AreEqual(1, dataContext.GameDetails.Count);
             Assert.AreEqual(1, dataContext.Participations.Count);
+        }
+
+        [TestMethod]
+        public void ValidClients()
+        {
+            Client client = dataContext.Clients[0];
+            Assert.AreEqual(1213, client.ClientNumber);
+            Assert.AreEqual("Steve", client.FirstName);
+            Assert.AreEqual("Works", client.LastName);
         }
     }
 }
