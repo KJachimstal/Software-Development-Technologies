b1a8ce60dd27a813e147efc8b4a1451f0de1c7dc
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-20 12:34:42 +0200

    Add participations list class

---
+++ Casino/CasinoSources/ParticipationsList.cs
@@ -0,0 +1,22 @@
+﻿using CasinoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CasionSources
+{
+    [XmlRoot("ParticipationsList")]
+    class ParticipationsList
+    {
+        public ParticipationsList()
+        {
+            Participations = new List<Participation>();
+        }
+
+        [XmlElement("Participation")]
+        public List<Participation> Participations { get; set; }
+    }
+}
