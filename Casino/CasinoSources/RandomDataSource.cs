eabbde488f9b3ffbb1ec59be47bb2e37fecc74f5
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-20 16:05:03 +0200

    Initialize random data source

---
+++ Casino/CasinoSources/RandomDataSource.cs
@@ -0,0 +1,40 @@
+﻿using CasinoData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasionSources
+{
+    class RandomDataSource : IDataSource
+    {
+        public void Fill(DataContext dataContext)
+        {
+            FillClients();
+            FillGames();
+            FillGamesDetails();
+            FillParticipations();
+        }
+
+        private void FillClients()
+        {
+
+        }
+
+        private void FillGames()
+        {
+
+        }
+
+        private void FillGamesDetails()
+        {
+
+        }
+
+        private void FillParticipations()
+        {
+
+        }
+    }
+}
