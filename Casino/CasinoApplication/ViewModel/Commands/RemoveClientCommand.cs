954a3f7a4bd544bf0f2794156dc1fb5827015479
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-22 11:02:00 +0200

    Add remove client command class

---
+++ Casino/CasinoApplication/ViewModel/Commands/RemoveClientCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CasinoApplication.ViewModel.Commands
+{
+    class RemoveClientCommand : ICommand
+    {
+        readonly Action<object> execute;
+        readonly Predicate<object> canExecute;
+
+        public RemoveClientCommand(Action<object> executeDelegate, Predicate<object> canExecuteDelegate)
+        {
+            execute = executeDelegate;
+            canExecute = canExecuteDelegate;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute(this);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(this);
+        }
+    }
+}
