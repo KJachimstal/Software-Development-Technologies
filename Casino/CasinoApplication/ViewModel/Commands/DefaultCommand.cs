893faf09cab6402238f7f350b5f3aa456852de19
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-23 00:42:23 +0200

    Add default command

---
+++ Casino/CasinoApplication/ViewModel/Commands/DefaultCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CasinoApplication.ViewModel.Commands
+{
+    class DefaultCommand : ICommand
+    {
+
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DefaultCommand(Action<object> executeDelegate, Predicate<object> canExecuteDelegate)
+        {
+            execute = executeDelegate;
+            canExecute = canExecuteDelegate;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute(parameter); 
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
