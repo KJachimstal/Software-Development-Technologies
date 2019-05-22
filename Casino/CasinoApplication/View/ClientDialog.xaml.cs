834b94bc8f6f9eae35d25dad368af53af3b8054d
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-22 19:12:00 +0200

    Change client dialog to user control

--- Casino/CasinoApplication/View/ClientDialog.xaml.cs
+++ Casino/CasinoApplication/View/ClientDialog.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CasinoApplication.View
@@ -17,7 +18,7 @@ namespace CasinoApplication.View
     /// <summary>
     /// Logika interakcji dla klasy ClientDialog.xaml
     /// </summary>
-    public partial class ClientDialog : Window
+    public partial class ClientDialog : UserControl
     {
         public ClientDialog()
         {
