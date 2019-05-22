87a8bb522d9bc17883043dd9a59a5e85c5f4f65c
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-22 18:46:41 +0200

    Add IDialog interface

---
+++ Casino/CasinoApplication/Interfaces/IDialog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoApplication.Interfaces
+{
+    interface IDialog
+    {
+        void AssignViewModel<TViewModel>(TViewModel viewModel);
+        void ShowDialog();
+        void Close();
+    }
+}
