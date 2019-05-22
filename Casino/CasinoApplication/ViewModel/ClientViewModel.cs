3d571f97c2c11d3af9871462e58e8a62bbb76e25
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-23 00:45:08 +0200

    Add cancel command to client view

--- Casino/CasinoApplication/ViewModel/ClientViewModel.cs
+++ Casino/CasinoApplication/ViewModel/ClientViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using CasinoApplication.Common;
+using CasinoApplication.ViewModel.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CasinoApplication.ViewModel
 {
@@ -21,6 +24,20 @@ namespace CasinoApplication.ViewModel
         public string LastName {
             get { return lastName; }
             set { lastName = value; }
+        }
+
+        private ICommand cancelCommand;
+
+        public ICommand CancelCommand 
+        {
+            get 
+            {
+                if (cancelCommand == null)
+                {
+                    cancelCommand = new DefaultCommand(e => { MessageBox.Show("Cancel"); }, null);
+                }
+                return cancelCommand;
+            }
         }
 
 
