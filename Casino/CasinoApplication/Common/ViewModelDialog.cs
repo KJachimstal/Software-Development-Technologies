736a8c611ee603a34f87160cb9982aceb9353dbc
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-22 19:06:33 +0200

    Add view model dialog to client view model

--- Casino/CasinoApplication/Common/ViewModelDialog.cs
+++ Casino/CasinoApplication/Common/ViewModelDialog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,13 @@ namespace CasinoApplication.Interfaces
 {
     public abstract class ViewModelDialog
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public enum Mode { ADD, EDIT }
         public Mode DialogMode { get; set; }
     }
