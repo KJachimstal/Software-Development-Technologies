146cc89e5163124fb3adf89fd225c42e733cc3f9
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-22 12:45:32 +0200

    Add command for adding objects

---
+++ Casino/CasinoApplication/ViewModel/Commands/AddCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CasinoApplication.ViewModel.Commands
+{
+    class AddCommand : ICommand
+    {
+        private Action<object> execute;
+
+        public AddCommand(Action<object> executeDelegate)
+        {
+            execute = executeDelegate;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(this);
+        }
+    }
+}
