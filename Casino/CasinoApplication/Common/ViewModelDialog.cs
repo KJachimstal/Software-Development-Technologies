dd6c283348fbd05b90a700d84ff49d1b749ab9d1
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-22 19:03:54 +0200

    Add view model dialog abstract class

---
+++ Casino/CasinoApplication/Common/ViewModelDialog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoApplication.Interfaces
+{
+    public abstract class ViewModelDialog
+    {
+        public enum Mode { ADD, EDIT }
+        public Mode DialogMode { get; set; }
+    }
+}
