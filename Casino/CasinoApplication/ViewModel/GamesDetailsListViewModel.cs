41591c993a0724705b0d543a3bf95b4696dcdd91
k.jachimstal <k.jachimstal@gmail.com>
2019-05-23 21:26:49 +0200

    Add gamesDetails collection

--- Casino/CasinoApplication/ViewModel/GamesDetailsListViewModel.cs
+++ Casino/CasinoApplication/ViewModel/GamesDetailsListViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using CasinoLibrary;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CasinoApplication.ViewModel
 {
-    class GamesDetailsListViewModel
+    public partial class MainViewModel : ViewModel
     {
+        private ObservableCollection<GameDetails> gamesDetails;
+
+        public ObservableCollection<GameDetails> GamesDetails {
+            get { return gamesDetails; }
+            set { gamesDetails = value; }
+        }
+
     }
 }
