a75807ef46eecc8224259273956874e9adcbc113
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-23 18:25:54 +0200

    Add game view dialog

---
+++ Casino/CasinoApplication/View/GameViewDialog.cs
@@ -0,0 +1,44 @@
+﻿using CasinoApplication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoApplication.View
+{
+    public class GameViewDialog : IModalDialog
+    {
+        private GameView view;
+
+        public void BindViewModel<TViewModel>(TViewModel viewModel)
+        {
+            GetDialog().DataContext = viewModel;
+        }
+
+        public void Close()
+        {
+            GetDialog().Close();
+        }
+
+        public void ShowDialog()
+        {
+            GetDialog().Show();
+        }
+
+        private GameView GetDialog()
+        {
+            if (view == null)
+            {
+                view = new GameView();
+                view.Closed += new EventHandler(view_Closed);
+            }
+            return view;
+        }
+
+        void view_Closed(object sender, EventArgs e)
+        {
+            view = null;
+        }
+    }
+}
