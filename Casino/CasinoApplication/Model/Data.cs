2454edc740a1b8b8bd843150c74cf7a7c4c2207f
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-23 10:53:15 +0200

    Add data class

---
+++ Casino/CasinoApplication/Model/Data.cs
@@ -0,0 +1,31 @@
+﻿using CasinoData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoApplication.Model
+{
+    public static class Data
+    {
+        private static DataRepository dataRepository;
+
+        public static DataRepository DataRepository {
+            get => dataRepository;
+        }
+
+        private static DataService dataService;
+
+        public static DataService DataService {
+            get => dataService;
+        }
+
+        public static void RegisterDataRepository(DataRepository _dataRepository)
+        {
+            dataRepository = _dataRepository;
+            dataRepository.DataContext = new DataContext();
+            dataService = new DataService(dataRepository);
+        }
+    }
+}
