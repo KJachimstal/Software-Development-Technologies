af2f7548ed7e810e484829cd802d3916bd191bba
k.jachimstal <k.jachimstal@gmail.com>
2019-05-23 22:24:54 +0200

    Complete GameDetailsProvider

--- Casino/CasinoApplication/Services/GameDetailsProvider.cs
+++ Casino/CasinoApplication/Services/GameDetailsProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CasinoApplication.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace CasinoApplication.Services
 {
     class GameDetailsProvider
     {
+        public static IServiceLocator Instance { get; private set; }
+
+        public static void RegisterServiceLocator(IServiceLocator s)
+        {
+            Instance = s;
+        }
     }
 }
