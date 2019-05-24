d52297fd80e3ce13bca686cb9b79b01e09ca0374
k.jachimstal <k.jachimstal@gmail.com>
2019-05-24 03:06:50 +0200

    Add AddParticipation method

--- Casino/CasinoApplication/ViewModel/ParticipationsListViewModel.cs
+++ Casino/CasinoApplication/ViewModel/ParticipationsListViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CasinoLibrary;
+﻿using CasinoApplication.Interfaces;
+using CasinoApplication.Services;
+using CasinoApplication.ViewModel.Commands;
+using CasinoLibrary;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -18,7 +21,66 @@ namespace CasinoApplication.ViewModel
                 participations = value;
                 OnPropertyChanged("GamesDetailsList");
             }
-        }   
+        }
 
+        //private Participation selectedParticipation;
+
+        //public Participation SelectedParticipation {
+        //    get { return selectedParticipation; }
+        //    set {
+        //        selectedParticipation = value;
+        //        EditParticipationCommand.RaiseCanExecuteChanged();
+        //        RemoveParticipationCommand.RaiseCanExecuteChanged();
+        //    }
+        //}
+
+        // ------------------------ Comands
+        //private AddCommand addParticipationCommand;
+
+        //public AddCommand AddParticipationCommand {
+        //    get {
+        //        if (addParticipationCommand == null)
+        //        {
+        //            addParticipationCommand = new AddCommand(e => AddParticipations());
+        //        }
+        //        return addParticipationCommand;
+        //    }
+        //}
+
+        //private EditCommand editParticipationCommand;
+
+        //public EditCommand EditParticipationCommand {
+        //    get {
+        //        if (editParticipationCommand == null)
+        //        {
+        //            editParticipationCommand = new EditCommand(e => EditGameDetails(SelectedParticipation), e => SelectedParticipation != null);
+        //        }
+        //        return editParticipationCommand;
+        //    }
+        //}
+
+        //private RemoveCommand removeParticipationCommand;
+
+        //public RemoveCommand RemoveParticipationCommand {
+        //    get {
+        //        if (removeParticipationCommand == null)
+        //        {
+        //            removeParticipationCommand = new RemoveCommand(e => RemoveGameDetails(SelectedParticipation), e => SelectedParticipation != null);
+        //        }
+        //        return removeParticipationCommand;
+        //    }
+        //}
+
+        //----------------- Action
+        private void AddParticipation()
+        {
+            ParticipationViewModel viewModel = new ParticipationViewModel();
+            viewModel.Mode = Common.Mode.ADD;
+
+            IModalDialog dialog = ParticipationProvider.Instance.Get<IModalDialog>();
+            viewModel.SetCloseAction(e => dialog.Close());
+            dialog.BindViewModel(viewModel);
+            dialog.ShowDialog();
+        }
     }
 }
