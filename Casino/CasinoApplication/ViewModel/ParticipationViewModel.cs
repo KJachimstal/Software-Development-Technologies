85f90c747b8455b4487225769b250d7612d5009f
k.jachimstal <k.jachimstal@gmail.com>
2019-05-24 02:28:29 +0200

    Add gameDetails property

--- Casino/CasinoApplication/ViewModel/ParticipationViewModel.cs
+++ Casino/CasinoApplication/ViewModel/ParticipationViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CasinoApplication.Model;
+using CasinoLibrary;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,14 @@ namespace CasinoApplication.ViewModel
 {
     class ParticipationViewModel
     {
+        private ObservableCollection<GameDetails> gameDetails;
+
+        public ObservableCollection<GameDetails> GameDetails {
+            get {
+                gameDetails = Data.DataRepository.GetAllGameDetails();
+                return gameDetails;
+            }
+
+        }
     }
 }
