ee7249c83552cf5bdb1fe1024d5f08816aa831ea
k.jachimstal <k.jachimstal@gmail.com>
2019-05-24 02:57:05 +0200

    Add participations property

--- Casino/CasinoApplication/ViewModel/ParticipationsListViewModel.cs
+++ Casino/CasinoApplication/ViewModel/ParticipationsListViewModel.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using CasinoLibrary;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CasinoApplication.ViewModel
 {
-    class ParticipationsListViewModel
+    public partial class MainViewModel : ViewModel
     {
+        private ObservableCollection<Participation> participations;
+
+        public ObservableCollection<Participation> Participations {
+            get => participations;
+            set {
+                participations = value;
+                OnPropertyChanged("GamesDetailsList");
+            }
+        }   
+
     }
 }
