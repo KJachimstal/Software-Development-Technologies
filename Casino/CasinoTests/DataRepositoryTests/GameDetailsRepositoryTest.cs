350270c731f48764da92eeb842be2f510400b18a
k.jachimstal <k.jachimstal@gmail.com>
2019-05-24 22:12:35 +0200

    Initialize test class GameDetailsReposotoryTest

---
+++ Casino/CasinoTests/DataRepositoryTests/GameDetailsRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CasinoData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CasinoTests.DataRepositoryTests
+{
+    [TestClass]
+    public class GameDetailsRepositoryTest
+    {
+        DataRepository dataRepository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            IDbContext dbContext = new TestContext();
+            dataRepository = new DataRepository(dbContext);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
