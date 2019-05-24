6019c348b5c3110e43344f4a277ca0a87bd8441a
k.jachimstal <k.jachimstal@gmail.com>
2019-05-24 22:00:27 +0200

    Initialize test GameRapositoryTest

---
+++ Casino/CasinoTests/DataRepositoryTests/GameRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CasinoTests.DataRepositoryTests
+{
+    [TestClass]
+    public class GameRepositoryTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            IDbContext dbContext = new TestContext();
+            dataRepository = new DataRepository(dbContext);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
