30d06467e3c3673935d1d139a08f8ecf4e4d5320
k.jachimstal <k.jachimstal@gmail.com>
2019-05-24 02:17:47 +0200

    Implement interface IModalDialog

--- Casino/CasinoApplication/View/ParticipationViewDialog.cs
+++ Casino/CasinoApplication/View/ParticipationViewDialog.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CasinoApplication.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace CasinoApplication.View
 {
-    class ParticipationViewDialog
+    public class ParticipationViewDialog : IModalDialog
     {
+        public void BindViewModel<TViewModel>(TViewModel viewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
