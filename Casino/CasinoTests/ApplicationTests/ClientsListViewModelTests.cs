0c5c33ea02bc3e0ff68a1bbc02c461fcb90fe1c0
k.jachimstal <k.jachimstal@gmail.com>
2019-05-25 14:54:41 +0200

    Initialize tests for class ClientsListViewModel

---
+++ Casino/CasinoTests/ApplicationTests/ClientsListViewModelTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CasinoApplication.Model;
+using CasinoData;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CasinoTests.ApplicationTests
+{
+    [TestClass]
+    public class ClientsListViewModelTests
+    {
+        DataRepository dataRepository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            IDbContext dbContext = new TestContext();
+            dataRepository = new DataRepository(dbContext);
+            Data.RegisterDataRepository(dataRepository);
+        }
+
+        [TestMethod]
+        public void ClientsListAddTest()
+        {
+        }
+
+        [TestMethod]
+        public void ClientsListUpdateTest()
+        {
+        }
+
+        [TestMethod]
+        public void ClientsListRemoveTest()
+        {
+        }
+    }
+}
