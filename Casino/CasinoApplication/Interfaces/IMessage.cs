d693bb5077dd322ad45e4d6ce658814ec181a8c1
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-25 18:10:34 +0200

    Add IMessage interface

---
+++ Casino/CasinoApplication/Interfaces/IMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CasinoApplication.Interfaces
+{
+    interface IMessage
+    {
+        void Show(string title, string message, MessageBoxButton buttons, MessageBoxImage image);
+    }
+}
