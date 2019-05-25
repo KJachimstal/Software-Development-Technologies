7d4252e4aa6c5ad87b352d6d7ab11c664e901849
k.jachimstal <k.jachimstal@gmail.com>
2019-05-25 14:38:45 +0200

    Complete tests for class GameDetailsViewModelTests

--- Casino/CasinoTests/ApplicationTests/GameDetailsViewModelTests.cs
+++ Casino/CasinoTests/ApplicationTests/GameDetailsViewModelTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using CasinoApplication.Model;
+using CasinoApplication.ViewModel;
 using CasinoData;
+using CasinoLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CasinoTests.ApplicationTests
@@ -18,6 +21,38 @@ namespace CasinoTests.ApplicationTests
             Data.RegisterDataRepository(dataRepository);
         }
 
-        
+        [TestMethod]
+        public void GameDetailsAddViewModelTest()
+        {
+            Game game = new Game(3, "Russian", Game.GameType.ROULETTE);
+            GameDetails gameDetails = new GameDetails();
+            GameDetailsViewModel gameDetailsViewModel = new GameDetailsViewModel(game, gameDetails);
+            gameDetailsViewModel.Mode = CasinoApplication.Common.Mode.ADD;
+
+            gameDetailsViewModel.SetAddAction(e => { });
+            gameDetailsViewModel.SetCloseAction(e => { });
+            gameDetailsViewModel.OnSave();
+
+            // Assertion
+            Assert.AreEqual(3, Data.DataRepository.GetAllGameDetails().Count());
+
+        }
+
+        [TestMethod]
+        public void GameDetailsUpdateViewModelTest()
+        {
+            Game game = new Game(3, "Crazy Russian", Game.GameType.ROULETTE);
+            GameDetails gameDetails = Data.DataRepository.GetGameDetails(1);
+            GameDetailsViewModel gameDetailsViewModel = new GameDetailsViewModel(game, gameDetails);
+            gameDetailsViewModel.Mode = CasinoApplication.Common.Mode.EDIT;
+
+            gameDetailsViewModel.SetAddAction(e => { });
+            gameDetailsViewModel.SetCloseAction(e => { });
+            gameDetailsViewModel.OnSave();
+
+            // Assertion
+            Assert.AreEqual("Crazy Russian", Data.DataRepository.GetGameDetails(1).Game.Name);
+
+        }
     }
 }
