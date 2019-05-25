93a5ebc3f0974c089c7eeea265fc60b83687dd21
Patryk Janicki <kontakt.janicki@gmail.com>
2019-05-25 18:15:41 +0200

    Add default message class

---
+++ Casino/CasinoApplication/Common/DefaultMessage.cs
@@ -0,0 +1,18 @@
+﻿using CasinoApplication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CasinoApplication.Common
+{
+    class DefaultMessage : IMessage
+    {
+        public void Show(string message, string title, MessageBoxButton buttons, MessageBoxImage image)
+        {
+            MessageBox.Show(message, title, buttons, image);
+        }
+    }
+}
