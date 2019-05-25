59eaa5ab41a73688189bdb0aed758db094ff6419
k.jachimstal <k.jachimstal@gmail.com>
2019-05-25 14:46:19 +0200

    Complete tests for class ParticipationViewModel

--- Casino/CasinoTests/ApplicationTests/ParticipationViewModelTests.cs
+++ Casino/CasinoTests/ApplicationTests/ParticipationViewModelTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using CasinoApplication.Model;
+using CasinoApplication.ViewModel;
 using CasinoData;
+using CasinoLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CasinoTests.ApplicationTests
@@ -19,8 +22,40 @@ namespace CasinoTests.ApplicationTests
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void ParticipationAddViewModelTest()
         {
+            Client client = new Client(4, "Wilhelm", "Pa");
+            GameDetails gameDetails = new GameDetails();
+            Participation participation = new Participation();
+            ParticipationViewModel participationViewModel = new ParticipationViewModel(client, gameDetails, participation);
+            participationViewModel.Mode = CasinoApplication.Common.Mode.ADD;
+
+            participationViewModel.SetAddAction(e => { });
+            participationViewModel.SetCloseAction(e => { });
+            participationViewModel.OnSave();
+
+            // Assertion
+            Assert.AreEqual(3, Data.DataRepository.GetAllParticipations().Count());
+
+        }
+
+        [TestMethod]
+        public void ParticipationUpdateViewModelTest()
+        {
+            Client client = Data.DataRepository.GetParticipation(1).Client;
+            GameDetails gameDetails = Data.DataRepository.GetParticipation(1).GameDetails;
+            Participation participation = Data.DataRepository.GetParticipation(1);
+            client.FirstName = "Wilhelm";
+            ParticipationViewModel participationViewModel = new ParticipationViewModel(client, gameDetails, participation);
+            participationViewModel.Mode = CasinoApplication.Common.Mode.ADD;
+
+            participationViewModel.SetAddAction(e => { });
+            participationViewModel.SetCloseAction(e => { });
+            participationViewModel.OnSave();
+
+            // Assertion
+            Assert.AreEqual("Wilhelm", Data.DataRepository.GetParticipation(1).Client.FirstName);
+
         }
     }
 }
